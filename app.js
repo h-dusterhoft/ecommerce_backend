1dc44e76dfba0838ed77bdc16c88b810c58bb7a9
H Dusterhoft <119828145+h-dusterhoft@users.noreply.github.com>
2023-07-18 20:38:09 -0500

    Require bodyParser and cors

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000; // ADD process.env.PORT later
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.get('/', (req, res) => {
     res.send('Welcome to my e-commerce site!');
