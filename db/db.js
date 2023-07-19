8cb8d768418a69e7c780ec157d741947c5708b87
H Dusterhoft <119828145+h-dusterhoft@users.noreply.github.com>
2023-07-19 15:13:11 -0500

    Edit db pool.

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
-  database: 'ecommerce',
-  password: 'dbtestpw',
-  port: 5432,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 })
